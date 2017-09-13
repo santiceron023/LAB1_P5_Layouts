46156b834689b5f93fca720c4d696e5f46923c0d
santiceron023 <santiceron023@gmail.com>
2017-09-13 08:34:58 -0500

    Initial commit

---
+++ app/src/main/java/com/udea/santiagoceron/lab1_p5/MainActivity.java
@@ -0,0 +1,13 @@
+package com.udea.santiagoceron.lab1_p5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
